fe04a93520b805450a01ea324cd903d62d4e0351
IlyaRomahin <31959971+IlyaRomahin@users.noreply.github.com>
2022-06-27 16:10:56 +0700

    Add files via upload

---
+++ Network2/cmake-build-debug/CMakeFiles/Network2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Network2.dir/main.cpp.o"
+  "CMakeFiles/Network2.dir/Server.cpp.o"
+  "Network2.pdb"
+  "Network2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Network2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
